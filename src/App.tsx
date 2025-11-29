18dfc80bd0903b1e475e3e5eb0c937e03dd019ae
Enzo Costa <enzo_costa7@hotmail.com>
2025-11-29 14:08:30 -0300

    feat: setting react query api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
 export function App() {
@@ -10,7 +12,9 @@ export function App() {
 			<ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
 				<Helmet titleTemplate="%s | pizza.shop" />
 				<Toaster richColors />
-				<RouterProvider router={router}></RouterProvider>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router}></RouterProvider>
+				</QueryClientProvider>
 			</ThemeProvider>
 		</HelmetProvider>
 	);
