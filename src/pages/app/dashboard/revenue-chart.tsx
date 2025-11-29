0c9927395b64b402ba6e1f6571482d24eb9ab077
Enzo Costa <enzo_costa7@hotmail.com>
2025-11-29 13:12:09 -0300

    feat: create the most products sold chart

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -46,6 +46,8 @@ export function RevenueChart() {
 							}
 						/>
 
+						<CartesianGrid vertical={false} className="stroke-muted" />
+
 						<Line type="linear" strokeWidth={2} dataKey="revenue" stroke={colors.violet["500"]} />
 					</LineChart>
 				</ResponsiveContainer>
